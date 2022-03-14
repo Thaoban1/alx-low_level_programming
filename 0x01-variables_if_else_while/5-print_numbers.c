47f4ea838ee0ed9e9b6bd4e81737128d75eeed71
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-13 19:00:34 -0700

    asa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main
+ *
+ * Return joor
+ */
+int main (void)
+
+{
+int num;
+
+for (num =0; num < 10; num++)
+{
+printf("%num", num);
+}
+
+print("\n");
+
+return (0);
+
+}
