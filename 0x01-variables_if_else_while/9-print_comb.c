96a9325a714ca860ac113f1cd0161192715fe6ec
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-13 19:58:19 -0700

    ger

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int num;
+
+for (num = 48; num <= 57; num++0
+{
+putchar (num);
+
+if (num == 57)
+{
+break;
+}
+
+putchar (',');
+putchar (' ');
+}
+
+putchar ('\n');
+
+return (0);
+
+}
