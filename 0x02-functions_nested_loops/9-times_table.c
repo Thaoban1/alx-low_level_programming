1784e5311e982b5f3cb938da3d08a71851f6364c
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-16 15:52:47 -0700

    aas

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
 	}
-if (b < 9)
+	   if (b < 9)
 							{
 							_putchar (',');
 							_putchar (' ');
@@ -36,7 +36,7 @@ else
 {
 	_putchar ((c / 10) + '0');
 	_putchar ((c % 10) + '0');
-							if (b < 9)
+	if (b < 9)
 							{				_putchar (',');
 	_putchar (' ');
 							}
