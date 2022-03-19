6c6a26f7001491d2c9d4587d3dbfd06c76d4898e
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-18 17:52:45 -0700

    sdds

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,27 @@
-#includ "main.h"
+#include "main.h"
 /**
  * main - main
- * Rewturn: 0
+ *
+ * Return: 0
  */
-
 int main(void)
 {
 int i;
+
+printf("Infinite loop incoming :(\n");
+
 i = 0;
 
-positive_or_negative(i);
+/*
+ * while (i < 10)
+ * {
+ * Infinite loop - No increase of variable
+ * putchar(i);
+ * }
+ */
 
+printf("Infinte loop avoided! \\o/\n");
 return (0);
-
 }
+
+
