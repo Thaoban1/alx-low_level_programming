3c535711e948331af2a2f12030abbc6518382a1d
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-18 20:51:20 -0700

    ddssppllkkjjjjddd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,24 @@
-#include "main.h"                                                                 
-                                                                          
-/**                                                                               
- * print_most_numbers - print numbers                                             
- *
- * Return: Always 0.                                                              
- */                                                                               
+#include "main.h"
 
-void print_most_numbers(void)                                                     
-{                                                                                 
-                                                                                  
-int i;                                                                            
-                                                                            
-i = 0;                                                                            
-while (i < 10)                                                                    
-{                                                                                 
-if (i != 2 && i != 4)                                                             
-{                                                                                 
-_putchar (i + '0');                                                               
-}                                                                                 
-i++;                                                                              
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+
+int i;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
 }
 
 _putchar ('\n');
