59d9ab0a7cdb20eb1e6103acf5d406d49163a092
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-21 13:25:45 -0700

    fasd

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,25 +1,29 @@
+
 #include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check your code
  *
  * Return: Always 0.
  */
 
-void swap(int *a, int *b);
-int main() {
+void swap_int(int *a, int *b);
+
+int main()
+{
 	int m = 10, n = 20;
 
-
 	swap(&m, &n);
+
 	return 0;
 }
 
-void swap(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int temp;
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
