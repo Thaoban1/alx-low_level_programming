f3b3bf696e03a9791a3f87fce065583b3d84e2ec
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-21 07:54:50 -0700

    gft

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int n = 40;
+void func (int *pn);
+*&pn = 98;
+
+int main () 
+{
+	_putchar (n);
+}
