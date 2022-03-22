4d43e0653321df78c2fb781d1edcf37334af0acb
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-21 20:27:16 -0700

    tyu

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - function to print
+ * @c: pointer to string
+ * Return: Always 0.
+ * On error, -1 is returned
+ */
+
+void _puts(char *str)
+{
+return (write(1, &c, 1));
+}
+
