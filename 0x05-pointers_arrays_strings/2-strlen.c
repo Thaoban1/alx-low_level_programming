8445c3cc977c1c27e33173c0b8b03b33169ec9b0
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-21 20:12:43 -0700

    tyu

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - to return lenth 
+ *@s: ponter to string to return
+
+ * Return: Always success
+ */
+
+int _strlen(char *s)
+{
+	int n;
+	n = 0;
+	while(s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
