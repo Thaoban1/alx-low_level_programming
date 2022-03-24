01c9625b1d80410a5f0f19dfdbd46c8b1f1fe72d
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-24 09:14:49 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - function to concat string
+ * @dest: destination of the strung
+ * @src: source of the string
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, scr);
+}
