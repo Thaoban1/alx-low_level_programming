c643802c16787c11040456e7be530802cc521537
Thaoban1 <saobanjokodola8@gmail.com>
2022-03-29 10:18:42 -0700

    tyyio

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,29 +5,37 @@
  * @haystack: pointer to char
  * @needle: pointer to char
  * Return: 0
+ * print_chessboard - prints chessboard.
+ * @a: matrix
  */
 
 char *_strstr(char *haystack, char *needle)
+	void print_chessboard(char (*a)[8])
 {
 	char *result = haystack, *fneedle = needle;
+	int i, j;
 
 	while (*haystack)
-	{
-		while (*needle)
+		for (i = 0; i < 8; i++)
 		{
-			if (*haystack++ != *needle++)
+			while (*needle)
+				for (j = 0; j < 8; j++)
+				{
+					if (*haystack++ != *needle++)
+					{
+						break;
+					}
+				}
+			if (!*needle)
 			{
-				break;
+				return (result);
+				_putchar(*(*(i + a) + j));
 			}
+			needle = fneedle;
+			result++;
+			haystack = result;
+			_putchar('\n');
 		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
-	}
 	return (0);
 }
 
