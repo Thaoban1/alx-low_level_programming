c25a4a54b1b9b713801acdf8901645f79fae8d86
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-01 16:39:03 -0700

    fds

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -67,7 +67,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
