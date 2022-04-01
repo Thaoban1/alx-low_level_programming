f1fd11fe14bf9d2b27eea386f2a616a371a8f022
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-01 14:54:10 -0700

    ffd

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,12 +6,14 @@
  * main - to print the numver
  * @argc: number of agrs
  * @argv: agrument vectors
+ * Description: to print
  * Return: always success
  */
 
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
+	argv = argv;
 
 	return (0);
 }
