0ed9767fcfa816432b0aafc814d9ffa509248497
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-01 14:31:26 -0700

    ttty

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - to print the numver
+ * @argc: number of agrs
+ * @argv: agrument vectors
+ * Return: always success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
+
