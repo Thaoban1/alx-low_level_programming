1d2e87f1bd6fc5f6a096f32931d01a4a7bfd19b5
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-01 15:05:10 -0700

    ff

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to print all args
+ * @argc: numver of args
+ * @argv: args
+ * Return: always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+
+}
+
