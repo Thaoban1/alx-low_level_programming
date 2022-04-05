eea937b0910c303135020b947e610a6fd028854d
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-05 01:40:48 -0700

    mallocv2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,10 @@
  */
 int _strlen(char *s)
 {
-	int i = 0;
-
-	while (*(s + i) != '\0')
-			i++;
-
-	return (i);
+int i = 0;
+while (*(s + i) != '\0')
+i++;
+return (i);
 }
 
 
@@ -31,34 +29,31 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
+int i, j, size1, size2, totSize;
+char *ar;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	int i, j, size1, size2, totSize;
-	char *ar;
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+totSize = (size1 + size2)+1;
+ar = malloc(totSize *sizeof(char));
 
-	if (s1 == NULL)
-			s1 = "";
-	if (s2 == NULL)
-			s2 = "";
-
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	totSize = (size1 + size2)+1;
-	ar = malloc(totSize *sizeof(char));
-
-	if (ar == NULL)
-			return (NULL);
-	else
-	{
-		for (i = 0; i < size1; i++)
-				ar[i] = s1[i];
-
-		for (j = 0; j < size2; j++)
-		{
-				ar[i] = s2[j];
-					i++;
-		}
-		ar[i + 1] = '\0';
-		return (ar);
-	}
+if (ar == NULL)
+return (NULL);
+else
+{
+for (i = 0; i < size1; i++)
+ar[i] = s1[i];
+for (j = 0; j < size2; j++)
+{
+ar[i] = s2[j];
+i++;
+}
+ar[i + 1] = '\0';
+return (ar);
+}
 
 }
