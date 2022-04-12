7c4fee0ffad3ca0dfb68300daf00ec7d3964c7cb
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-12 03:16:17 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: onwer of the fucking dog
+ */
+struct dog{
+
+	char *name;
+	float age;
+	char *owner;
+
+}
