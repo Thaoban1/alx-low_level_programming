77d9fb747e223242b3caa86b171e49d9fdadd17d
Thaoban1 <saobanjokodola8@gmail.com>
2022-04-14 15:40:20 -0700

    func

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,8 @@
-#include "function_pointers.h"
-
-/**
- * array_iterator - loops through an array
- * @array: pointer to array
- * @size: size of the array
- * @action: pointer to function
- * Return: nothing
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-unsigned int i;
-if (array != NULL && action != NULL)
-{
-for (i = 0; i < size; i++)
-action(array[i]);
-}
-}
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif}
 
